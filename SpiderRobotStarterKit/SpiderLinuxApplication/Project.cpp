65a70342c9eeea88c6631063e9baf9831386e347
SangYun <sang_yun-kim0125@hotmail.com>
2017-11-30 21:43:41 -0600

    First commit

--- SpiderRobotStarterKit/SpiderLinuxApplication/Project.cpp
+++ SpiderRobotStarterKit/SpiderLinuxApplication/Project.cpp
@@ -13,11 +13,70 @@
 #include "PIO_BUTTON.h"
 #include "ADC.h"
 
+
+#define MAX_IDLE_TIME 600000
+
+
+typedef enum{
+    CMD_AT,
+    CMD_FORWARD,
+    CMD_BACKWARD,
+    CMD_TURN_RIGHT,
+    CMD_TURN_LEFT,
+    CMD_TURN_RIGHT_DEGREE,
+    CMD_TURN_LEFT_DEGREE,
+    CMD_STOP,
+    CMD_SPEED,
+    CMD_TILTL,
+    CMD_TILTR,
+    CMD_TILTF,
+    CMD_TILTB,
+    CMD_TILTN,
+    CMD_Query_Version,
+    CMD_JOYSTICK,
+    CMD_ALL,
+    CMD_IDLE
+}COMMAND_ID;
+
+static void *bluetooth_spp_thread(void *ptr)
+{
+    CBtSppCommand BtSppCommand;
+    CQueueCommand *pQueueCommand;
+    int command, param;
+
+    pQueueCommand = (CQueueCommand*)ptr;
+    printf("[BT] Start Service\r\n");
+    BtSppCommand.RegisterService();
+
+    while (true)
+    {
+        printf("[BT] Listening...\r\n");
+        BtSppCommand.RfcommOpen();
+        printf("[BT] Connected.\r\n");
+        while (true)
+        {
+            command = BtSppCommand.CommandPolling(param);
+            if (command != CMD_IDLE)
+            {
+                if (command == CMD_STOP)
+                    pQueueCommand->Clear();
+
+                if (!pQueueCommand->isFull())
+                    pQueueCommand->Push(command, param);
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]){
 
-    printf("===== Group H Final Project =====\r\n");
-
     CSpider spider;
+    CQueueCommand command_queue;
+    int command, param;
+    uint32_t last_action;
+    bool asleep = false;
+
+    printf("===== Group H Final Project =====\r\n");
 
     printf("Spider initializing\r\n");
 
@@ -39,7 +98,7 @@ int main(int argc, char *argv[]){
     //ADC adc;
 
     printf("Creating BlueTooth thread.");
-    int thread_ret = pthread_create(&id0,NULL,bluetooth_spp_thread, (void *)&QueueCommand);
+    int thread_ret = pthread_create(&id0, NULL, bluetooth_spp_thread, (void *)&QueueCommand);
     if (thread_ret != 0)
     {
         printf("Failed to create pthread.");
@@ -47,6 +106,61 @@ int main(int argc, char *argv[]){
 
     printf("Listening for command...\r\n");
     LED_PIO.SetLED(0x7f); //Indicate on spider
+    last_action = OS_GetTickCount();
+
+    while(true)
+    {
+        if(!asleep && ((OS_GetTickCount()-last_action) > MAX_IDLE_TIME))
+        {
+            asleep = true;
+            last_action = OS_GetTickCount();
+            spider.Sleep();
+            LED_PIO.SetLED(0x1);
+        }
+
+        if (BUTTON_PIO.GetBUTTON() == 0x2)
+        {
+            if (asleep)
+            {
+                asleep = false;
+                spider.WakeUp();
+                LED_PIO.SetLED(0x7f);
+            }
+            spider.Reset();
+            last_action = OS_GetTickCount();
+        }
+        else if (BUTTON_PIO.GetBUTTON() == 0x1)
+        {
+            if (asleep)
+            {
+                asleep = false;
+                spider.WakeUp();
+                LED_PIO.SetLED(0x7f);
+            }
+            spider.DEMO_Dance(1);
+            last_action = OS_GetTickCount();
+        }
+
+        if (!command_queue.isEmpty() && command_queue.Pop(&command, &param))
+        {
+            if (asleep)
+            {
+                asleep = false;
+                spider.WakeUp();
+                LED_PIO.SetLED(0x7f);
+            }
+
+            switch(command)
+            {
+                case CMD_FORWARD:
+                    printf("FORWARD");
+                    spider.MoveForward(1);
+                    break;
+
+                default: printf("Nothing happens.");
+            }
+        }
+    }
 
 
 	return 0;
